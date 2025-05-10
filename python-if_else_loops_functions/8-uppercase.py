e2c42364c1806657dc89951858cc089c66b8ca17
Jin Liu <iamjincn@outlook.com>
2025-05-10 23:00:40 +1000

    task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            char = chr(ord(str[i]) - 32)
+        else:
+            char = str[i]
+        print("{}".format(char), end="")
