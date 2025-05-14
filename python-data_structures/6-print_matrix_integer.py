788e409e7ec3a327b118aafe8aadc20c217469b5
Jin Liu <iamjincn@outlook.com>
2025-05-14 19:09:27 +1000

    fix: correct print statement in print_matrix_integer function to properly format row output

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print("".join("{:d}".format(i) for i in row()))
+        print(" ".join("{:d}".format(i) for i in row))
+
 
 
         
