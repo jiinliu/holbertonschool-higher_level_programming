797f2840d04a83a8b87a3f30423111010220089f
Jin Liu <iamjincn@outlook.com>
2025-05-14 20:54:09 +1000

    fix: simplify divisible_by_2 function using list comprehension

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = []
-    
-    for i in range(len(my_list)):
-        new_list[i] = my_list[i] % 2 == 0
-            
-    return new_list
+    return [i % 2 == 0 for i in my_list]
