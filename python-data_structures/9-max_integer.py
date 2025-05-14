7dd808538da82da02c5cffbba14cbbf942451fac
Jin Liu <iamjincn@outlook.com>
2025-05-14 20:03:41 +1000

    fix: correct loop iteration in max_integer function to properly find maximum value

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     max_int = my_list[0]
-    for i in my_list:
+    for i in my_list[1:]:
         if i > max_int:
             max_int = i
         return max_int
