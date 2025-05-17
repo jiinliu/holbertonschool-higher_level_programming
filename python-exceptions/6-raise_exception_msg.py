6fe954489e03cef6879b4ed8efe7ad13834bb217
Jin Liu <iamjincn@outlook.com>
2025-05-17 22:48:12 +1000

    refactor: remove example usage comment from raise_exception_msg.py

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
     raise NameError(message)
-# Example usage:
 if __name__ == "__main__":
     try:
         raise_exception_msg("This is a custom error message")
