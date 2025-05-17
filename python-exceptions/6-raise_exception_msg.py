502e3d9dfed40ef408f5de96e7353cac7e6ce2ff
Jin Liu <iamjincn@outlook.com>
2025-05-17 22:49:37 +1000

    style: add blank lines for improved readability in raise_exception_msg.py

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
     raise NameError(message)
+
+
 if __name__ == "__main__":
     try:
         raise_exception_msg("This is a custom error message")
