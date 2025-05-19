b366b5bc7d87a0939e5bf4ed4476685a010b1e00
Jin Liu <iamjincn@outlook.com>
2025-05-19 16:54:32 +1000

    feat: implement Square class with initialization and documentation

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines a class named Square.
+"""
+
+
+class Square:
+    """
+    Class for Square.
+    """
+    def __init__(self, size):
+        """
+        Initialize the square with a given size.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
