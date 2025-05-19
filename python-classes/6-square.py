2d9ce01abff78c3af619f788a56a51d0b96972a1
Jin Liu <iamjincn@outlook.com>
2025-05-19 21:51:25 +1000

    fix: correct formatting in position setter method

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -71,7 +71,7 @@ class Square:
            not all(isinstance(i, int) and i >= 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-        
+
     def area(self):
         """
         Calculate the area of the square.
