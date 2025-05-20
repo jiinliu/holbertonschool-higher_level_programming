3f379770eff89530469a1331db240b81d2339a15
Jin Liu <iamjincn@outlook.com>
2025-05-20 19:38:15 +1000

    fix: correct indentation and spacing in __str__ method of Rectangle class

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -79,10 +79,9 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
     def __str__(self):
         """Return a string representation of the Rectangle."""
-        if self.__width ==0 or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return  ""
         lines = []
         for _ in range (self.__height):
             lines.append("#" * self.__width)
-            return "\n".join(lines)
-
+        return "\n".join(lines)
