3aea2367e90bfb3bf5bf70c2208236373762179d
Jin Liu <iamjincn@outlook.com>
2025-05-23 14:55:16 +1000

    fix: add a newline for better readability in Rectangle class

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
     Class for Rectangle.
     """
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
