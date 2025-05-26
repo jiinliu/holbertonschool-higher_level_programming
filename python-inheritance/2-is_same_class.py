4a819a264fedf5fcedefd240afb7b09a1269399e
Jin Liu <iamjincn@outlook.com>
2025-05-26 23:12:15 +1000

    fix: format docstring for consistency in is_same_class function

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,6 +6,6 @@ object is exactly an instance of a given class.
 
 
 def is_same_class(obj, a_class):
-    """Returns True if obj is exactly an
-    instance of a_class; otherwise False."""
+    """Returns True if obj is exactly an instance of a_class;
+    otherwise False."""
     return type(obj) is a_class
