8fce3a6c0a7fa2c075b6b8bd6ac11edfb5dfc39a
Jin Liu <iamjincn@outlook.com>
2025-05-26 15:31:39 +1000

    fix: correct class and method docstring in MyList class

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""
-This module defines an empty class named Rectangle.
+"""This module defines a class MyList that inherits from list.
 """
 
 
 class MyList(list):
-    """
-    A subclass of list with a methosd to print the list sorted.
-    """
+    """A subclass of list with a method to print the list sorted."""
 
-
-def print_sorted(self):
-    """
-    Prints the list in sorted order.
-    """
-    print(sorted(self))
+    def print_sorted(self):
+        """Prints the list, but sorted in ascending order."""
+        print(sorted(self))
