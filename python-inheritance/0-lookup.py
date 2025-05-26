adf1dc7c4b203aa092a7ebffd2c0dd1126a79f9a
Jin Liu <iamjincn@outlook.com>
2025-05-26 14:27:25 +1000

    fix: correct typos in module docstring and function docstring in lookup function

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+This module provides a function to list available attributes and metods of an abject.
+"""
 def lookup(obj):
     """
-    Returns a list of available attributes and methobds of an object."""
+    Returns a list of available attributes and methobds of an object.
+    Args:
+        obj: The object to inspect.
+    Returns:
+        A list of strings representing the attributes and methods of the object.
+    """
     return dir(obj)
-
