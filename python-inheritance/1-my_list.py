092d139d297f3f21b7fda7b02c3ff848e3d49b61
Jin Liu <iamjincn@outlook.com>
2025-05-26 15:25:02 +1000

    fix: correct typos in class and function docstrings in MyList class

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class named Rectangle.
+"""
+
+
+class MyList(list):
+    """
+    A subclass of list with a methosd to print the list sorted.
+    """
+
+
+def print_sorted(self):
+    """
+    Prints the list in sorted order.
+    """
+    print(sorted(delf))
