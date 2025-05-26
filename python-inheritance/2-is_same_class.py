9bb576834d6139a257f12113a959ce97cca9f1cb
Jin Liu <iamjincn@outlook.com>
2025-05-26 23:10:57 +1000

    fix: format docstring for improved readability in is_same_class function

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 #!/usr/bin/python3
-"""This module provides a function to check if an object is exactly an instance of a given class.
+"""This module provides a function to check if an
+object is exactly an instance of a given class.
 """
 
 
 def is_same_class(obj, a_class):
-    """Returns True if obj is exactly an instance of a_class; otherwise False."""
+    """Returns True if obj is exactly an
+    instance of a_class; otherwise False."""
     return type(obj) is a_class
