de4bc8d183e0a7ac05eb40f0fccef47b4d899f49
Jin Liu <iamjincn@outlook.com>
2025-05-27 12:33:44 +1000

    feat: add BaseGeometry class with initial area method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#/usr/bin/python3
+"""This module defines a base geometry class."""
+
+
+class BaseGeometry:
+    """BaseGeometry clsss for geometry operations."""
+    
+    
+    def area(self):
+        """Raises an Exception for area."""
+        raise Exception("area() is not implemented")
