3773c7cb274af4bdf0580f1897504bad6d35d512
Jin Liu <iamjincn@outlook.com>
2025-05-31 19:43:33 +1000

    fix: streamline __next__ method in CountedIterator for clarity and efficiency

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -2,21 +2,19 @@
 class CountedIterator:
     """wrap any iterable and count how many items have been yielded."""
     
-
     def __init__(self, iterable):
         self._iter = iter(iterable)
         self._count = 0
         
-        
         def __iter__(self):
             return self
         
-        
         def __next__(self):
             item = next(self._iter)
+            if item is None:
+                raise StopIteration
             self._count +=1
             return item
-        
         
         def get_count(self):
             return self._count
