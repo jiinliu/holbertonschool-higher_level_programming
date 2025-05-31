582c73adf1d0a2efb172e414810856c8d10b293b
Jin Liu <iamjincn@outlook.com>
2025-05-31 12:18:11 +1000

    feat: implement VerboseList class with custom append, extend, remove, and pop methods

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+class VerboseList(list):
+ """A short, chatty list."""
+ 
+ 
+ def apppend(self, item):
+     super().append(item)
+     print(f"Added [{itrem}] to the list.")
+     
+     
+     def extend(self, iterable):
+         items = list(iterable)
+         super().extend(items)
+         print(f"Extended the list with [{len(items}] items.")
+         
+         
+         def remove(self, item):
+             print(f"Removing [{item}] from the list.")
+             super().remove(item)
+             
+             
+def ppop(self, index=-1):
+    item = super().pop(index)
+    print(f"Popped [{item}] from the list.")
+    return item
+ 
