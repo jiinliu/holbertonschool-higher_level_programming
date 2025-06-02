01af80a673e9ab430aba7131d994e8c1bb43ae3c
Jin Liu <iamjincn@outlook.com>
2025-06-02 19:59:02 +1000

    fix: improve formatting of docstrings in append_write.py for better readability

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Module that appends a string to a text file (UTF8) and returns the number of characters added.
-"""
+"""Module that appends a string to a text file (UTF8)
+returns the number of characters added."""
 
 
 def append_write(filename="", text=""):
-    """Appends a string to a text file (UTF8) and returns the number of characters added.
-    """
+    """Appends a string to a text file (UTF8)
+    returns the number of characters added."""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
