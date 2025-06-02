ffd4f4588d5a52b63d19009a8cc320f27b8b0906
Jin Liu <iamjincn@outlook.com>
2025-06-02 18:00:08 +1000

    fix: correct formatting in docstring of read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,8 +4,9 @@
 
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout.
-    
+
     Args:
-        filename (str): The name of the file to read. Defaults to an empty string."""
+        filename (str): The name of the file to read.
+        Defaults to an empty string."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
