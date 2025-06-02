f33e5e715a2279fa5bb80c39a940ef622cb01f20
Jin Liu <iamjincn@outlook.com>
2025-06-02 18:23:46 +1000

    fix: improve formatting of module docstring in write_file.py for better readability

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Module that writes a string to a text file and returns the number of characters written."""
+"""Module that writes a string to a text file and
+returns the number of characters written."""
+
+
 def write_file(filename="", text=""):
     """Defines a function that writes a string to a text file (UTF8)
     returns the number of characters written."""
