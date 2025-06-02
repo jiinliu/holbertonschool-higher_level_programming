047b43a6221c32a6eea6779f7791daeb09ef90c3
Jin Liu <iamjincn@outlook.com>
2025-06-02 17:53:24 +1000

    feat: add read_file function to read and print text files

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a function that reads a text fiule (UTF8) and print it to stdout."""
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout.
+    
+    Args:
+        filename (str): The name of the file to read. Defaults to an empty string."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
