21d4bf304f3548e944e877dc33e672bcfdb8d3b0
Jin Liu <iamjincn@outlook.com>
2025-06-02 18:19:30 +1000

    fix: correct shebang and improve docstring formatting in write_file function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,6 @@
-#!/usr/bin//python3
-"""Defines a function that writes a string to a text file (UTF8) and returns the number of characters written."""
+#!/usr/bin/python3
 def write_file(filename="", text=""):
+    """Defines a function that writes a string to a text file (UTF8)
+    returns the number of characters written."""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
