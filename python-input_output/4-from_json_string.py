1bdbc5de90df7ffbed267b324b8dee3b6f5dfbe3
Jin Liu <iamjincn@outlook.com>
2025-06-04 15:51:00 +1000

    feat: add from_json_string function to convert JSON string to object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module to turns JSON string to object."""
+
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object represented by a JSON string.
+    
+    Args:
+        my_str (str): The JSON string."""
+    return json.loads(my_str)
