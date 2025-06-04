b8bc3f8404e3fb0fce4cd8c49496833ebacb57ba
Jin Liu <iamjincn@outlook.com>
2025-06-04 22:24:28 +1000

    fix: correct typo in docstring of load_from_json_file function

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,7 +5,7 @@ import json
 
 def load_from_json_file(filename):
     """Creates an object from a JSON file.
-    
+   
     Args:
         filename : The name of the file to read feom."""
     with open(filename, 'r', encoding='utf-8') as f:
