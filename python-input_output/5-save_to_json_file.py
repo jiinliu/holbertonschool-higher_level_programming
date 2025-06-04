61a7c1b78f797d730ff4027f71100a538340adf2
Jin Liu <iamjincn@outlook.com>
2025-06-04 16:10:35 +1000

    fix: correct typo in shebang line of 5-save_to_json_file.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,7 +1,9 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 """Module to save an object to a file"""
 
 import json
+
+
 def save_to_json_file(my_obj, filename):
     """Writes an object to a text file, using a JSON representation."""
     with open(filename, 'w', encoding='utf-8') as f:
