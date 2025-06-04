1bb1d6c05bac9f8d18d4b614c4b2407a1bb7b38b
Jin Liu <iamjincn@outlook.com>
2025-06-04 23:09:35 +1000

    fix: correct syntax error in main block of 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 """Module to create an object from a JSON file and add it to a file."""
 import sys
+
+
+if __name__ == "__main__":
+
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    file_name = "add_item.json"
+
+    json_file = load_from_json_file(file_name)
+    if json_file is None:
+        json_file = []
+
+    arg_list = sys.argv[1:]
+    json_file = json_file.append(arg_list)
+
+    save_to_json_file(json_file, file_name)
