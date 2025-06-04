4528638d616bbb30066160d7b2a719f2b399f737
Jin Liu <iamjincn@outlook.com>
2025-06-04 16:23:25 +1000

    feat: implement load_from_json_file function to deserialize an object from a JSON file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module Creates an objecct from a JSON file."""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an object from a JSON file.
+    
+    Args:
+        filename : The name of the file to read feom."""
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
