5192e796adf924297aa65106cfc729fe33e5effe
Jin Liu <iamjincn@outlook.com>
2025-06-05 21:40:45 +1000

    fix: remove unnecessary blank lines and adjust indentation in Student class

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -4,20 +4,20 @@
 
 class Student:
     """Represents a student."""
-    
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self, attrs=None):
         """Returns a dictionary representation of the Student instance.
-        
+
         If attrs is a list of strings, only those attributes are included.
         """
         if attrs is None:
             return self.__dict__
         if isinstance(attrs,
-                      list) and all(type(attr) == str for attr in attrs):
-	        return {attr: getattr(self,
+                    list) and all(type(attr) == str for attr in attrs):
+            return {attr: getattr(self,
                                attr) for attr in attrs if hasattr(self, attr)}
