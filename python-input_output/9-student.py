20ca7ec0c5cf5b9a3c777c91a04bb2cdc2834d43
Jin Liu <iamjincn@outlook.com>
2025-06-05 20:41:21 +1000

    fix: correct typo in constructor method name for Student class

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,7 +3,7 @@
 class Student:
     """A class that defines a student."""
     
-    def __innit__(self, first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
         """Initializes the student with first name, last name and age."""
         self.first_name = first_name
         self.last_name = last_name
