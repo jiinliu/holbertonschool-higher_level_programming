cb3a09ba92d0650a40ce46fbb231b7fc2a40a83a
Jin Liu <iamjincn@outlook.com>
2025-06-05 20:43:58 +1000

    fix: correct typo in docstring for to_json method and remove unnecessary whitespace

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,13 +5,13 @@
 class Student:
     """A class that defines a student."""
 
-    
+
     def __init__(self, first_name, last_name, age):
         """Initializes the student with first name, last name and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self):
         """Returns the dictionary reprsentation of the Student."""
         return self.__dict__
