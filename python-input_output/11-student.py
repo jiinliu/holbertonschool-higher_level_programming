6218f828ca4c921e3801f388294c95b50710eb7f
Jin Liu <iamjincn@outlook.com>
2025-06-05 22:21:17 +1000

    fix: correct typo in Student class docstring and remove redundant lines

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -4,15 +4,14 @@
 
 class Student:
     """Defines a student by first_name, last_neme, and age."""
-    
-    
+
     def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self, attrs=None):
-        """Return dictionart representation of the Student, optionallly filtered by attrs."""
         if attrs is None:
             return self.__dict__
         if isinstance(attrs, list) and all(isinstance(attr, str)
