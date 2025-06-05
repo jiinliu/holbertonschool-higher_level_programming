91ddbaa94ecf154f9464ba6fb70ed57f60698081
Jin Liu <iamjincn@outlook.com>
2025-06-05 20:18:12 +1000

    fix: format docstring for class_to_json function for better readability

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-"""Defines a function that returns the dictionary description for JSON serialization of an object."""
+"""Defines a function that returns the dictionary
+description for JSON serialization of an object."""
+
+
 def class_to_json(obj):
     return obj.__dict__
