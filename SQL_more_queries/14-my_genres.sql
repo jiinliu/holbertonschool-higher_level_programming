aa171b3ed732abd26d8fe0c40e1c646c81fcc6eb
Jin Liu <iamjincn@outlook.com>
2025-06-28 21:05:33 +1000

    feat: add query to retrieve genres for the TV show 'Dexter'

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- My genres
+SELECT DISTINCT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+where tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
