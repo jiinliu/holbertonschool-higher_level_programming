52874be941e792302a430fc5e3d46d1975f37c3a
Jin Liu <iamjincn@outlook.com>
2025-06-29 22:48:28 +1000

    fix: enforce binary comparison for state name in SQL query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+        "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC"
         .format(state_name_searched)
         )
 
