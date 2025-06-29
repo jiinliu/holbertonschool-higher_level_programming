171d0d468d2b70fdd7151129d99ba62665516688
Jin Liu <iamjincn@outlook.com>
2025-06-29 22:36:25 +1000

    fix: correct SQL syntax error in state filtering query

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""List all states from the datable hbtn_0e_0_usa."""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username, password, db_name, state_name_searched = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WEHRE name = '{}'ORDER BY id ASC".format(state_name_searched))
+
+    for row in cursor.fetchall():
+        print(row)
+
+    # Cleanup
+    cursor.close()
+    db.close()
