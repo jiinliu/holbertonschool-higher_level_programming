913fe7e78d81f139aa0992caddf1cdd3188fde8b
Jin Liu <iamjincn@outlook.com>
2025-06-30 23:34:26 +1000

    fix: update docstring for State class in model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,6 +9,9 @@ from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 class State(Base):
+    """
+    State class that links to the MySQL table 'states'.
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
