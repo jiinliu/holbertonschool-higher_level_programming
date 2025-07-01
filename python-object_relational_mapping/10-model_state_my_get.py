3002868dcf5a59badedf7b1e634f250038d897f3
Jin Liu <iamjincn@outlook.com>
2025-07-01 16:11:16 +1000

    fix: improve docstring formatting in 10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-prints the State object with the name passed as argument from the database hbtn_0e_6_usa
+prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
