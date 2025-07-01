8f085cfe8929ff2997d38a21e6830d3afbda1833
Jin Liu <iamjincn@outlook.com>
2025-07-01 15:51:20 +1000

    feat: add script to list all State objects containing 'a' from the database

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Lists all State objects that containing the latter a fronm the databasee hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    states_with_a = session.query(State).filter(
+        State.name.like('%a%')).order_by(State.id)
+
+    for state in states_with_a:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
