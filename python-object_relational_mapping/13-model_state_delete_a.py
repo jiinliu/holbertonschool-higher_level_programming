6a676c1aaf88da5e86a2e4da50ff3373a02a5b9d
Jin Liu <iamjincn@outlook.com>
2025-07-01 16:45:50 +1000

    style: adjust indentation for improved readability in delete script

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -19,10 +19,10 @@ if __name__ == "__main__":
     session = Session()
 
     states_to_delete = (
-    session.query(State)
-    .filter(State.name.like('%a%'))
-    .all()
-)
+        session.query(State)
+        .filter(State.name.like('%a%'))
+        .all()
+    )
     for state in states_to_delete:
         session.delete(state)
     session.commit()
