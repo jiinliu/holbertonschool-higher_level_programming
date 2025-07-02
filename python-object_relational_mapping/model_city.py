e21c00313e9c0b925651fe0794664e3abec8d822
Jin Liu <iamjincn@outlook.com>
2025-07-02 21:14:43 +1000

    feat: add City class for SQLAlchemy ORM mapping to the cities table

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Defines the City class for SQLAlchemy ORM mapping to the cities table.
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+class City(Base):
+    """City class that links to the MySQL table 'cities'."""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
