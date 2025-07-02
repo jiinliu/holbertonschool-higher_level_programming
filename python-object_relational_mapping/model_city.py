260c8179ac9a17bbe2b155a2ec6a1923d75213e0
Jin Liu <iamjincn@outlook.com>
2025-07-02 21:19:00 +1000

    style: add a blank line for improved readability in model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -5,6 +5,7 @@ Defines the City class for SQLAlchemy ORM mapping to the cities table.
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
+
 class City(Base):
     """City class that links to the MySQL table 'cities'."""
     __tablename__ = 'cities'
