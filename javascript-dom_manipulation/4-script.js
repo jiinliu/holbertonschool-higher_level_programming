bb9abe646aed371e6c271b29b8f6cde8046bd42b
Jin Liu <iamjincn@outlook.com>
2025-07-26 15:51:08 +1000

    feat: add HTML and JavaScript files for DOM manipulation exercises

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,6 @@
+document.querySelector('#add_item').onclick = function () {
+  const ul = document.querySelector('.my_list');
+  const li = document.createElement('li');
+  li.textContent = 'Item';
+  ul.appendChild(li);
+};
