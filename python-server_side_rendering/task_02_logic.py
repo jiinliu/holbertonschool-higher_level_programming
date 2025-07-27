53327cc00c6142c9a649ba1a49c7069ffadb85e5
Jin Liu <iamjincn@outlook.com>
2025-07-27 21:03:54 +1000

    feat: add Flask app and templates to display items from JSON

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import json
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+with open('items.json', 'r') as f:
+    json_data = json.load(f)
+    
+@app.route('/items')
+def items():
+    return render_template('items.html', items=json_data['items'])
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
